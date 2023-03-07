e1b0d24614633096862e3a8bb088389ebc04bc18
Rorschach3 <daniel_fhernandez@yahoo.com>
2023-03-06 17:19:37 -0800

    Feature 5 complete

--- projects/models.py
+++ projects/models.py
@@ -1,13 +1,12 @@
 from django.db import models
-from django.contrib.auth.models import User
-
+from django.conf import settings
 
 # Create your models here.
 class Project(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
     owner = models.ForeignKey(
-        User,
+        settings.AUTH_USER_MODEL,
         related_name="projects",
         on_delete=models.CASCADE,
         null=True
